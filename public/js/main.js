9b91480749f29017bde9b0b7eccc01671e00432a
Jordi Sala <jordism91@gmail.com>
2014-01-25 21:54:18 +0100

    added styles for chat sending and button

--- public/js/main.js
+++ public/js/main.js
@@ -8,37 +8,34 @@ $(document).ready(function() {
         }
         var chatLine = '<div class="' + className + '">[' + message.date + '] <strong style=\'color: ' + message.color + '\'>&lt;' + message.name + '&gt;</strong> ' + message.message;
 
-        $('.content').append(chatLine);
+        $('.chat-lines').append(chatLine);
     }
 
     socket.on('message', function(data) {
-        console.log(data);
         addMessage(data);
     });
 
     $('.send-chat-message').click(function() {
         socket.emit('message_to_send', $('.chat-input').val());
     });
+});
 
-    Twitch.init({clientId: '1uzx8xlados5eqn7sb0pexoyuzkc1g9'}, function(error, status) {
-        if (error) console.log(error);
+Twitch.init({clientId: '1uzx8xlados5eqn7sb0pexoyuzkc1g9'}, function(error, status) {
+    if (error) console.log(error);
 
-        if (status.authenticated) {
-            var token = Twitch.getToken();
+    if (status.authenticated) {
+        var token = Twitch.getToken();
 
-            var test = Twitch.api({method: 'user'}, function(error, user) {
-                if (error) console.log(error);
-                socket.emit('login', {'username': user.name, 'oauth': token});
-            });
-        }
-        else {
-            $('.twitch-connect').show();
-            $('.twitch-connect').click(function() {
-                Twitch.login({
-                    scope: ['user_read', 'chat_login']
-                });
-            });
-        }
+        var test = Twitch.api({method: 'user'}, function(error, user) {
+            if (error) console.log(error);
+            socket.emit('login', {'username': user.name, 'oauth': token});
+        });
+    }
+    else {
+        $('.twitch-connect').show();
+        $('.twitch-connect').click(function() {
+            Twitch.login({ scope: ['user_read', 'chat_login'] });
+        });
+    }
 
-    });
 });
