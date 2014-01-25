82d5024153b58e8d94597959011ee2762b425770
Jordi Sala <jordism91@gmail.com>
2014-01-25 18:27:47 +0100

    fixing problems with creating more clients

--- server.js
+++ server.js
@@ -48,7 +48,5 @@ io.sockets.on('connection', function (socket) {
 
         client = new irc.Client(configurations.serverAddress, data.username, options);
         client.say(configurations.channelName, "I'm a bot!");
-
-        socket.emit('message', client);
     });
 });
