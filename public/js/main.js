153e905e2485fce0fc0296dacc2864439428bc08
Jordi Sala <jordism91@gmail.com>
2014-02-11 19:35:20 +0100

    added visual to know when you are logged in

--- public/js/main.js
+++ public/js/main.js
@@ -10,8 +10,11 @@ Twitch.init({clientId: '1uzx8xlados5eqn7sb0pexoyuzkc1g9'}, function(error, statu
             if (error) console.log(error);
             twitchChat.sendCredentials(user.name, token);
         });
+        $('.chat-input').attr('placeholder', 'Chat about this Channel');
     }
     else {
+        $('.chat-input').attr('placeholder', 'Sign up or log in to chat');
+        $('.chat-input').attr('disabled', 'disabled');
         $('.twitch-connect').show();
         $('.twitch-connect').click(function() {
             Twitch.login({ scope: ['user_read', 'chat_login'] });
