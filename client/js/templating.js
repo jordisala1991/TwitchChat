509478092eeb722176e27009dd3f2cafd520b9f4
Jordi Sala <jordism91@gmail.com>
2016-03-27 23:55:05 +0200

    fixing emoticon size

--- client/js/templating.js
+++ client/js/templating.js
@@ -10,11 +10,11 @@ Templating.prototype.userTemplating = function(message) {
 
     if (message.user === undefined) return '';
 
-    template = template.replace("{USER_COLOR}", message.user.color);
-    template = template.replace("{USER_NAME}", message.user.display_name);
-    template = template.replace("{DATE}", message.time);
-    template = template.replace("{USER_BADGES_ICONS}", message.user.badges);
-    template = template.replace("{SENDER}", message.user.user_name);
+    template = template.replace('{USER_COLOR}', message.user.color);
+    template = template.replace('{USER_NAME}', message.user.display_name);
+    template = template.replace('{DATE}', message.time);
+    template = template.replace('{USER_BADGES_ICONS}', message.user.badges);
+    template = template.replace('{SENDER}', message.user.user_name);
 
     return template;
 }
@@ -26,43 +26,32 @@ Templating.prototype.messageTemplating = function(message) {
             '<span class="message" style="color: {MESSAGE_COLOR}">{MESSAGE}</span>' +
         '</div>';
 
-    template = template.replace("{MESSAGE}", message.processed_message);
-    template = template.replace("{MESSAGE_COLOR}", message.color);
+    template = template.replace('{MESSAGE}', message.processed_message);
+    template = template.replace('{MESSAGE_COLOR}', message.color);
 
     return template;
 }
 
 Templating.prototype.emoticonTemplating = function(id, name) {
     var template =
-        '<span class="emoticon tooltip" data-tooltip-name="{NAME}" style="' +
-            'background-image: url(//static-cdn.jtvnw.net/emoticons/v1/{ID}/1.0); ' +
-            'height: 24px; ' +
-            'width: 24px; ' +
-            'margin: -5px 0px;">' +
+        '<span class="tooltip" data-tooltip-name="{NAME}">' +
+            '<img class="emoticon" src="//static-cdn.jtvnw.net/emoticons/v1/{ID}/1.0" srcset="//static-cdn.jtvnw.net/emoticons/v1/{ID}/2.0 2x" alt="{NAME}" />' +
         '</span>';
 
-    template = template.replace("{ID}", id);
-    template = template.replace("{NAME}", name);
+    template = template.replace(new RegExp('{ID}', 'g'), id);
+    template = template.replace(new RegExp('{NAME}', 'g'), name);
 
     return template;
 }
 
 Templating.prototype.subscriberTemplating = function(url) {
-    var template =
-        '<span class="badge" style="' +
-            'background-image: url({BACKGROUND_IMAGE});">' +
-        '</span>';
+    var template = '<span class="badge" style="background-image: url({BACKGROUND_IMAGE});"></span>';
 
-    template = template.replace("{BACKGROUND_IMAGE}", url);
-
-    return template;
+    return template.replace('{BACKGROUND_IMAGE}', url);
 }
 
 Templating.prototype.badgeTemplating = function(badge) {
-    var template =
-        '<span class="badge {BADGE}"></span>';
+    var template = '<span class="badge {BADGE}"></span>';
 
-    template = template.replace("{BADGE}", badge);
-
-    return template;
+    return template.replace('{BADGE}', badge);
 }
